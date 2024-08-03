e80ae6afe2ddaf58a3907148f6231a0e1b026724
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2024-08-03 20:21:55 +0900

    enqueue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,7 +1,15 @@
-import LinkedList from '../linked-list/LinkedList';
+import LinkedList from "../linked-list/LinkedList";
 
 export default class Queue {
-  constructor() {
-    this.linkedList = new LinkedList();
-  }
+	constructor() {
+		this.linkedList = new LinkedList();
+	}
+
+	enqueue(value) {
+		this.linkedList.append(value);
+	}
+
+	toString(callback) {
+		return this.linkedList.toString(callback);
+	}
 }
