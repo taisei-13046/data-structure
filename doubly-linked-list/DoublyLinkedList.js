0994d923f04748303ead42300fed657e29fff4e2
安井大晟 <taisei_yasui@yasui-taiseiMacBook-Air.local>
2024-08-03 16:45:48 +0900

    append

--- doubly-linked-list/DoublyLinkedList.js
+++ doubly-linked-list/DoublyLinkedList.js
@@ -14,4 +14,58 @@ export default class DoublyLinkedList {
 
     this.compare = new Comparator(comparatorFunction);
   }
+
+  /**
+   * @param {*} value
+   * @return {DoublyLinkedList}
+   */
+  append(value) {
+    const newNode = new DoublyLinkedListNode(value);
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    newNode.previous = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  /**
+   * @return {DoublyLinkedListNode[]}
+   */
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  /**
+   * @param {*[]} values - Array of values that need to be converted to linked list.
+   * @return {DoublyLinkedList}
+   */
+  fromArray(values) {
+    values.forEach((value) => this.append(value));
+
+    return this;
+  }
+
+  /**
+   * @param {function} [callback]
+   * @return {string}
+   */
+  toString(callback) {
+    return this.toArray()
+      .map((node) => node.toString(callback))
+      .toString();
+  }
 }
--- doubly-linked-list/__test__/DoublyLinkedList.test.js
+++ doubly-linked-list/__test__/DoublyLinkedList.test.js
@@ -6,19 +6,19 @@ describe("DoublyLinkedList", () => {
     expect(linkedList.toString()).toBe("");
   });
 
-  //it("should append node to linked list", () => {
-  //  const linkedList = new DoublyLinkedList();
+  it("should append node to linked list", () => {
+    const linkedList = new DoublyLinkedList();
 
-  //  expect(linkedList.head).toBeNull();
-  //  expect(linkedList.tail).toBeNull();
+    expect(linkedList.head).toBeNull();
+    expect(linkedList.tail).toBeNull();
 
-  //  linkedList.append(1);
-  //  linkedList.append(2);
+    linkedList.append(1);
+    linkedList.append(2);
 
-  //  expect(linkedList.head.next.value).toBe(2);
-  //  expect(linkedList.tail.previous.value).toBe(1);
-  //  expect(linkedList.toString()).toBe("1,2");
-  //});
+    expect(linkedList.head.next.value).toBe(2);
+    expect(linkedList.tail.previous.value).toBe(1);
+    expect(linkedList.toString()).toBe("1,2");
+  });
 
   //it("should prepend node to linked list", () => {
   //  const linkedList = new DoublyLinkedList();
