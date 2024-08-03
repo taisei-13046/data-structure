f3dfdbdcc3ab16c957b5afea578626208aa3ba77
安井大晟 <taisei_yasui@yasui-taiseiMacBook-Air.local>
2024-08-03 16:36:36 +0900

    init

---
+++ doubly-linked-list/DoublyLinkedList.js
@@ -0,0 +1,17 @@
+import DoublyLinkedListNode from "./DoublyLinkedListNode";
+import Comparator from "../utils/Comparator";
+
+export default class DoublyLinkedList {
+  /**
+   * @param {Function} [comparatorFunction]
+   */
+  constructor(comparatorFunction) {
+    /** @var DoublyLinkedListNode */
+    this.head = null;
+
+    /** @var DoublyLinkedListNode */
+    this.tail = null;
+
+    this.compare = new Comparator(comparatorFunction);
+  }
+}
